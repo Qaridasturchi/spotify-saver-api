b46c28c7e826b1a69402dfdda8f201b945b98563
Qaridasturchi <154114591+Qaridasturchi@users.noreply.github.com>
2023-12-23 20:27:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ from shazamio import Shazam
 
 from fastapi.encoders import jsonable_encoder
 
+from spotify_search import SearchFromSpotify
 
+from funcs import DownloadMusic
 
 import requests
 
@@ -254,7 +256,29 @@ async def search_track(query: str, limit: int = 10):
 
 
 
+@app.get("/download-music/")
 
+async def download_music(track_name: str = Query(..., title="Track Name", description="Name of the track to search"),
+
+                         limit: int = Query(..., title="Limit", description="Limit the number of search results")):
+
+    try:
+
+        track_urls = SearchFromSpotify(track_name, limit)
+
+        audio_urls = DownloadMusic(track_urls)
+
+
+
+
+
+
+
+        return {"downloaded_audio_urls": audio_urls}
+
+    except Exception as e:
+
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 
