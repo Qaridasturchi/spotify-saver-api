36e26cd32e7928189a6bc87f8d357f6d414abb36
Qaridasturchi <154114591+Qaridasturchi@users.noreply.github.com>
2023-12-23 20:10:01 +0500

    Update spotify_search.py

--- spotify_search.py
+++ spotify_search.py
@@ -1,8 +1,8 @@
 import spotipy,json
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = 'your_spotify_clent_id_here'
-client_secret = 'your_spotify_client_secrect_here'
+client_id = '93dfe93f21fb46aaad10947eadfde562'
+client_secret = '36bb6e205ff143c6aeab7c3eb9bafd3a'
 
 # If you have any questions about client_id and client_secret, welcome to ask Telegram: @ai_junior
 
